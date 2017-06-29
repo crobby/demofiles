3ce1e841e9a90299322630746f61129091fc35da
Chad Roberts <croberts@redhat.com>
2017-06-29 09:46:14 -0400

    Updated for more configs

--- democonfigs/make_test_configs.sh
+++ democonfigs/make_test_configs.sh
@@ -1,11 +1,14 @@
-#/bin/bash
-oc delete configmap masterconfig workerconfig clusterconfig setworkers
+#!/bin/bash
+oc delete configmap masterconfig masternorp workerconfig clusterconfig setworkers
 
 oc create configmap masterconfig --from-file=masterconfig_test
+
+oc create configmap masternorp --from-file=masterconfig_no_rp
 
 oc create configmap workerconfig --from-file=workerconfig_test
 
 oc create configmap clusterconfig --from-literal=sparkmasterconfig=masterconfig \
-                                  --from-literal=sparkworkerconfig=workerconfig
+                                  --from-literal=sparkworkerconfig=workerconfig \
+                                  --from-literal=workercount=3
 
 oc create configmap setworkers --from-literal=workercount=5
